dd457a6d20c31960d96ebb80ba30b600ab64620c
LyoSU <mail@lyo.su>
2019-12-15 02:05:23 +0200

    fix

--- utils/quote-generate.js
+++ utils/quote-generate.js
@@ -482,8 +482,6 @@ module.exports = async (backgroundColor, message, replyMessage, entities) => {
   let nameColor = nameColorBlack[nameMap[nameIndex]]
   if (backStyle === 'light') nameColor = nameColorLight[nameMap[nameIndex]]
 
-  console.log(message)
-
   const nameSize = 22
 
   let nameCanvas
