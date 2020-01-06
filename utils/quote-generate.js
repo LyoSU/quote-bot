99d8b181257bba2e5193de17cdfcf10f51480ea8
LyoSU <mail@lyo.su>
2020-01-06 23:55:43 +0200

    scale change

--- utils/quote-generate.js
+++ utils/quote-generate.js
@@ -431,7 +431,7 @@ async function drawQuote (scale = 1, backgroundColor, avatar, replyName, replyTe
 }
 
 module.exports = async (backgroundColor, message, replyMessage, entities, width = 512, height = 512) => {
-  const scale = 2.5
+  const scale = 2
 
   width *= scale
   height *= scale
