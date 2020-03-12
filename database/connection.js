0dab5c4b3d332799bb20017829908486ca194c00
LyoSU <ua.lyo.su@gmail.com>
2020-03-12 12:29:13 +0200

    0.10.7

--- database/connection.js
+++ database/connection.js
@@ -5,8 +5,4 @@ const connection = mongoose.createConnection(process.env.MONGODB_URI, {
   useNewUrlParser: true
 })
 
-connection.catch((error) => {
-  console.log('DB error', error)
-})
-
 module.exports = connection
--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 require('dotenv').config({ path: './.env' })
 require('./bot')
+
+process.on('unhandledRejection', (err) => {
+  console.error('Uhandled Rejection:', err)
+  process.exit(-1)
+})
