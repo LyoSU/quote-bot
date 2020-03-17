3fd51372e96c862c47caf6ab6c7c12bb0e8010a4
Yuri Ly <mail@lyo.su>
2020-03-17 19:53:01 +0200

    disable restart

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config({ path: './.env' })
 require('./bot')
 
-process.on('unhandledRejection', (err) => {
-  console.error('Uhandled Rejection:', err)
-  process.exit(-1)
-})
+#process.on('unhandledRejection', (err) => {
+#  console.error('Uhandled Rejection:', err)
+#  process.exit(-1)
+#})
