6b9ac295c8a4ba08b52cdb007a048a2a9cda2c94
Yuri Ly <mail@lyo.su>
2021-02-10 17:28:54 +0200

    Update index.js

--- helpers/tdlib/index.js
+++ helpers/tdlib/index.js
@@ -13,7 +13,7 @@ const client = new Client(new TDLib(`${tdDirectory}/${tdLibFile}`), {
   apiHash: process.env.TELEGRAM_API_HASH || '68875f756c9b437a8b916ca3de215815',
   databaseDirectory: `${tdDirectory}/db`,
   filesDirectory: tdDirectory,
-  logVerbosityLevel: 0
+  verbosityLevel: 0
 })
 
 client.on('error', console.error)
