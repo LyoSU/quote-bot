b4fe957413ccefdeaa89c7e48d59971e9e0e8a7d
Yuri Ly <mail@lyo.su>
2021-04-25 18:22:09 +0300

    maxTimeMS

--- database/connection.js
+++ database/connection.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 const connection = mongoose.createConnection(process.env.MONGODB_URI, {
   poolSize: 100,
+  maxTimeMS: 3,
   useUnifiedTopology: true,
   useNewUrlParser: true
 })
