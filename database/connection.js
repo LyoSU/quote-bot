a555a311722a052062086a6d9064648a998f10e7
Yuri Ly <mail@lyo.su>
2021-04-25 18:25:13 +0300

    fix

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connection = mongoose.createConnection(process.env.MONGODB_URI, {
-  poolSize: 100,
+  poolSize: 10,
   maxTimeMS: 3,
   useUnifiedTopology: true,
   useNewUrlParser: true
