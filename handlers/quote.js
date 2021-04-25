f1273711c50ad46076cb50be704322da11dbf235
Yuri Ly <mail@lyo.su>
2021-04-25 17:54:36 +0300

    0.23.23 remove privacy check

--- handlers/quote.js
+++ handlers/quote.js
@@ -278,13 +278,13 @@ module.exports = async (ctx) => {
 
     console.log(`⏰ ${ctx.message.message_id} 3.4 + ${index} >>>`, new Date() - timeStartGen, 'ms')
 
-    if (!flag.privacy && message.from) {
-      if (ctx.group && ctx.group.info.settings.privacy && !ctx.chat.username) flag.privacy = true
-      else {
-        // const quotedFind = await ctx.db.User.findOne({ telegram_id: message.from.id })
-        // if (quotedFind && quotedFind.settings.privacy) flag.privacy = true
-      }
-    }
+    // if (!flag.privacy && message.from) {
+    //   if (ctx.group && ctx.group.info.settings.privacy && !ctx.chat.username) flag.privacy = true
+    //   else {
+    //     const quotedFind = await ctx.db.User.findOne({ telegram_id: message.from.id })
+    //     if (quotedFind && quotedFind.settings.privacy) flag.privacy = true
+    //   }
+    // }
 
     console.log(`⏰ ${ctx.message.message_id} 3.5 + ${index} >>>`, new Date() - timeStartGen, 'ms')
 
