289760f9bd1a6e57161eaac36d3944db90b795f0
Yuri Ly <mail@lyo.su>
2021-06-10 17:27:12 +0300

    0.28.3 add json command

--- bot.js
+++ bot.js
@@ -92,6 +92,8 @@ bot.use((ctx, next) => {
   return next()
 })
 
+bot.command('json', ({ replyWithHTML, message }) => replyWithHTML('<code>' + JSON.stringify(message, null, 2) + '</code>'))
+
 bot.use((ctx, next) => {
   ctx.db = db
   return next()
