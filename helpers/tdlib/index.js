ca907248e9bd0ac3f516ff007d0a6b150223956a
Yuri Ly <mail@lyo.su>
2021-11-08 22:26:31 +0200

    0.30.1 fix media

--- helpers/tdlib/index.js
+++ helpers/tdlib/index.js
@@ -249,7 +249,7 @@ function getMessages (chatID, messageIds) {
                         width: size.width
                       }
                     })
-                  } else if (messageInfo.content[type].thumbnail) {
+                  } else if (type === 'video' && type === 'animation' && messageInfo.content[type].thumbnail) {
                     const { file } = messageInfo.content[type].thumbnail
                     media = {
                       thumbnail: {
