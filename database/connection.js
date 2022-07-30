ff38ed1e38a7adfc3f7ab77f151bba6955aa61de
Yuri Ly <mail@lyo.su>
2022-07-31 00:43:53 +0300

    0.34.7 exit app when error

--- database/connection.js
+++ database/connection.js
@@ -9,6 +9,7 @@ const connection = mongoose.createConnection(process.env.MONGODB_URI, {
 
 connection.on('error', error => {
   console.error(error)
+  process.exit(1)
 })
 
 module.exports = connection
