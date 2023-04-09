e162ef4b4e0d0b5f4db3b395619fd58f9ad49a9a
Yuri Ly <yuri@lyubchak.com>
2023-04-09 16:53:13 +0300

    0.36.14 optimize emoji handler

--- handlers/sticker.js
+++ handlers/sticker.js
@@ -20,27 +20,29 @@ module.exports = async (ctx, next) => {
   } else if (ctx.message.entities || ctx.message.entities) {
     const entities = ctx.message.entities || ctx.message.caption_entities
 
-    entities.forEach(async (entity) => {
+    const customEmoji = entities.map((entity) => {
       if (entity.type === 'custom_emoji') {
-        const emojiStickers = await ctx.telegram.callApi('getCustomEmojiStickers', {
-          custom_emoji_ids: [entity.custom_emoji_id]
-        })
-
-        if (emojiStickers.length > 0) {
-          const sticker = emojiStickers[0]
-
-          if (!sticker.set_name) {
-            return
-          }
-
-          got.post(process.env.FSTIK_API_URI + '/publishStickerSet?token=' + process.env.BOT_TOKEN, {
-            json: {
-              name: sticker.set_name
-            }
-          }).catch(() => {})
-        }
+        return entity.custom_emoji_id
       }
-    })
+    }).filter((entity) => entity)
+
+    if (customEmoji.length > 0) {
+      const emojiStickers = await ctx.telegram.callApi('getCustomEmojiStickers', {
+        custom_emoji_ids: customEmoji
+      })
+
+      emojiStickers.forEach((sticker) => {
+        if (!sticker.set_name) {
+          return
+        }
+
+        got.post(process.env.FSTIK_API_URI + '/publishStickerSet?token=' + process.env.BOT_TOKEN, {
+          json: {
+            name: sticker.set_name
+          }
+        }).catch(() => {})
+      })
+    }
   }
 
   return next()
