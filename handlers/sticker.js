3f9a4c15f8cccbddd40b72418f7d65ad9532d0e1
Andrii <tap.press.swap@gmail.com>
2024-10-06 16:50:25 +0300

    add redis to dev docker profile

--- handlers/sticker.js
+++ handlers/sticker.js
@@ -1,7 +1,10 @@
 const got = require('got')
 const Redis = require('ioredis')
 
-const redis = new Redis()
+const redis = new Redis({
+  host: process.env.REDIS_HOST || "127.0.0.1",
+  port: process.env.REDIS_PORT || 6379
+})
 
 const PREFIX = 'quotly'
 
