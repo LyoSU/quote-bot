482549dacbaf615411128793750174b89421dc36
Yuri Ly <yuri@lyubchak.com>
2024-10-17 20:06:52 +0300

    Update mongoose connection options

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
 const connection = mongoose.createConnection(process.env.MONGODB_URI, {
+  poolSize: 10,
+  maxTimeMS: 3,
   useUnifiedTopology: true,
   useNewUrlParser: true
 })
