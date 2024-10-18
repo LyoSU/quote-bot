ffa059082eaa7036402bf3157fdc0e063b2ca263
Yuri Ly <yuri@lyubchak.com>
2024-10-18 15:29:56 +0300

    fix :))))

--- helpers/group-get.js
+++ helpers/group-get.js
@@ -1,52 +1,42 @@
 module.exports = async ctx => {
-  const { db, chat, i18n, session } = ctx
+  const { db, chat, i18n } = ctx
   const now = Math.floor(Date.now() / 1000)
 
-  if (!session.groupInfo) {
-    const updateData = {
-      title: chat.title,
-      username: chat.username,
-      updatedAt: new Date()
-    }
-
-    const group = await db.Group.findOneAndUpdate(
-      { group_id: chat.id },
-      {
-        $set: updateData,
-        $setOnInsert: { first_act: now }
-      },
-      {
-        new: true,
-        upsert: true,
-        setDefaultsOnInsert: true
-      }
-    )
-
-    if (chat && chat.type === 'group') {
-      group.status = 'active'
-    }
-
-    session.groupInfo = group
-  } else {
-    Object.assign(session.groupInfo, {
-      title: chat.title,
-      username: chat.username,
-      updatedAt: new Date()
-    })
-
-    if (chat && chat.type === 'group') {
-      session.groupInfo.status = 'active'
-    }
+  const updateData = {
+    title: chat.title,
+    username: chat.username,
+    updatedAt: new Date()
   }
 
-  if (session.groupInfo.settings.locale) {
-    i18n.locale(session.groupInfo.settings.locale)
-  } else {
-    const locale = i18n.shortLanguageCode || i18n.languageCode
-    if (locale) {
-      i18n.locale(locale)
-      session.groupInfo.settings.locale = locale
-      await session.groupInfo.save()
+  const group = await db.Group.findOneAndUpdate(
+    { group_id: chat.id },
+    {
+      $set: updateData,
+      $setOnInsert: {
+        first_act: now,
+        settings: new db.Group().settings
+      }
+    },
+    {
+      new: true,
+      upsert: true,
+      setDefaultsOnInsert: true,
+      returnDocument: 'after'
+    }
+  )
+
+  if (chat && chat.type === 'group') {
+    group.status = 'active'
+  }
+
+  ctx.group.info = group
+
+  const locale = group.settings.locale || i18n.shortLanguageCode || i18n.languageCode
+  if (locale) {
+    i18n.locale(locale)
+    if (!group.settings.locale) {
+      group.settings.locale = locale
+      await group.save()
     }
   }
 
