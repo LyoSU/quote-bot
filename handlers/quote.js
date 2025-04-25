0c3808f0c7eb06dc2e83de991760f515173509db
pacificescape <17690123+pacificescape@users.noreply.github.com>
2025-04-25 15:24:32 +0200

    fix: HTTP-Referer fixed

--- handlers/quote.js
+++ handlers/quote.js
@@ -20,7 +20,7 @@ const openai = new OpenAI({
   baseURL: "https://openrouter.ai/api/v1",
   apiKey: process.env.OPENAI_API_KEY,
   defaultHeaders: {
-    "HTTP-Referer": "hppt://t.me/quotlybot",
+    "HTTP-Referer": "https://quotlybot.t.me/",
     "X-Title": "Quotly Bot",
   }
 })
